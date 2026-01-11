ba37aac797ceaf2ede8b1819381abcab6474ec4a
Golan <goland1@gmail.com>
2026-01-11 20:51:48 +0200

    Stop gracefully step added

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -28,5 +28,5 @@ module "gke" {
   pods_range_name     = "pods"
   services_range_name = "services"
 
-  service_account = module.iam.google_service_account.gke_nodes.email
+  service_account = module.iam.google_service_account.gke_nodes
 }
