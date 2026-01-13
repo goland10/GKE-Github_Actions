cd944dda5019c8b8ee186fc6ebacaf6a273e3625
Golan <goland1@gmail.com>
2026-01-13 22:38:56 +0200

    Get GKE credentials

--- terraform/gcp/outputs.tf
+++ terraform/gcp/outputs.tf
@@ -2,12 +2,13 @@ output "cluster_name" {
   value = module.gke.cluster_name
 }
 
-output "debug_env" {
-  value = var.environment
+output "location" {
+  description = "GKE cluster location (zone for dev, region for staging/prod)"
+  value       = local.location
 }
 
-output "debug_location" {
-  value = local.location
+output "debug_env" {
+  value = var.environment
 }
 
 output "debug_node_locations" {
